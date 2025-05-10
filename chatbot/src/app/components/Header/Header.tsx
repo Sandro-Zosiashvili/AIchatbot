c9961daeedc9bf9189830b110ff6831a7fc643c5
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2025-05-10 20:16:03 +0400

    create header

---
+++ chatbot/src/app/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import styles from './Header.module.scss'
+
+
+
+const Header = () => {
+
+
+    return (
+        <div  className={styles.container}>
+            <div className={styles.header}>
+                AI AGENTS
+            </div>
+        </div>
+
+    )
+}
+
+export default Header
--- chatbot/src/app/page.tsx
+++ chatbot/src/app/page.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Logo from "./components/Logo/Logo";
 import Menu from "./components/Menu/Menu";
+import Header from "./components/Header/Header";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
+
 
 
 
