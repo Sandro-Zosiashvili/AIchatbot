4082b9a15c5260001636d649d4973ce0aa45acdf
sandro zosiashvili <157837817+Sandro-Zosiashvili@users.noreply.github.com>
2025-06-18 22:15:23 +0400

    create contact us  connection on bakcend

--- chatbot/src/app/contactus/page.tsx
+++ chatbot/src/app/contactus/page.tsx
@@ -1,43 +1,83 @@
+'use client'; // Add this at the very top of the file
 
+import Button from '../components/Button/Button';
+import styles from './page.module.scss';
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from 'axios';
 
-import Button from '../components/Button/Button'
-import styles from './page.module.scss'
-
-
-
+type FormData = {
+    email: string;
+    phoneNumber: string;
+    company: string;
+    socialMedia: string
+    aboutProjct: string
+    terms: string
+};
 
 export default function Contactus() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>();
+
+    const onSubmit: SubmitHandler<FormData> = (data) => {
+        axios.post("http://localhost:3002/contact", data)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error("Error:", error.response?.data || error.message);
+            });
+    };
 
     return (
         <div className={styles.container}>
-
             <div className={styles.aboutusHeader}>
                 Contact <span className={styles.different}>Us</span>
             </div>
-            <div className={styles.mainInputWrapper}>
+            <form onSubmit={handleSubmit(onSubmit)} className={styles.mainInputWrapper}>
                 <div className={styles.inputWrapper}>
-                    <input className={styles.inputMail} type='email' placeholder='Enter your E-mail' />
+                    <input
+                        className={styles.inputMail}
+                        {...register("email", { required: true })}
+                        type='email'
+                        placeholder='Enter your E-mail'
+                    />
+                    {errors.email && <span>This field is required</span>}
                 </div>
 
                 <div className={styles.inputWrapper}>
-                    <input className={styles.inputMail} type='text' placeholder='Mobile Number' />
+                    <input className={styles.inputMail}
+                        {...register("phoneNumber", { required: true })}
+                        type='text' placeholder='Mobile Number' />
                 </div>
 
                 <div className={styles.inputWrapper}>
-                    <input className={styles.inputMail} type='text' placeholder='Company’s name or your name' />
+                    <input className={styles.inputMail}
+                        {...register("company", { required: true })}
+
+                        type='text' placeholder='Company' />
                 </div>
 
                 <div className={styles.inputWrapper}>
-                    <input className={styles.inputMail} type='text' placeholder='Web site or social media link' />
+                    <input className={styles.inputMail}
+                        {...register("socialMedia", { required: true })}
+
+                        type='text' placeholder='Web site or social media link' />
                 </div>
 
                 <div className={styles.inputWrapperTwo}>
-                    <textarea className={styles.inputAbout} placeholder='Web site or social media link' />
+                    <textarea className={styles.inputAbout}
+                        {...register("aboutProjct", { required: true })}
+                        placeholder='Tell us about your project' />
                 </div>
 
                 <div className={styles.checkBoxAccept}>
                     <div className={styles.checkboxWrapper}>
-                        <input type="checkbox" className={styles.checkbox} />
+                        <input type="checkbox"
+                            {...register("terms", { required: true })}
+                            className={styles.checkbox} />
                     </div>
                     <div className={styles.fontAccept}>
                         I accept terms and conditions.
@@ -46,11 +86,7 @@ export default function Contactus() {
                 <div className={styles.button}>
                     <Button title={'Send'} />
                 </div>
-            </div>
-
-
+            </form>
         </div>
-
-    )
+    );
 }
-
